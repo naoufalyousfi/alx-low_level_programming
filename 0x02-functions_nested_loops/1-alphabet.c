d0a6b2d451b4292060e1a56de8f7c9d034133af4
naoufalyousfi <naoufalyousfi@gmail.com>
2023-06-21 12:30:43 -0700

    afiichage de lower cases

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_alphabet(void)
+{
+    char letter = 'a';
+
+    while (letter <= 'z')
+    {
+        _putchar(letter);
+        letter++;
+    }
+
+    _putchar('\n');
+}
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -4,3 +4,4 @@ int _putchar(char c)
 {
     return putchar(c);
 }
+
